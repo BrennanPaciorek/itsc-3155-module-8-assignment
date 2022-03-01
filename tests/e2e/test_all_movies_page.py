ce1b72b6d8eef3b6838fe770a8d9d437b696733e
BrennanP <brenp5744@hotmail.com>
2022-03-01 00:10:56 -0500

    test_all_movies_page 'name' field switched to 'title' to reflect movie object fields

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -2,9 +2,9 @@
 import pytest
 
 def test_list_all_movies(test_app):
-    test_app.post('/movies',data=dict(name="Spiderman", director="Sam Raimi", rating="3"))
-    test_app.post('/movies', data=dict(name="Spiderman 2",director="John Watts", rating="4"))
-    test_app.post('/movies', data=dict(name="Popular Movie", director="Director Person", rating="1"))
+    test_app.post('/movies',data=dict(title="Spiderman", director="Sam Raimi", rating="3"))
+    test_app.post('/movies', data=dict(title="Spiderman 2",director="John Watts", rating="4"))
+    test_app.post('/movies', data=dict(title="Popular Movie", director="Director Person", rating="1"))
     response = test_app.get('/movies')
     assert b"<tr><th>Spiderman</th><th>Sam Raimi</th><th>3/5</th></tr>" in response.data
     assert b"<tr><th>Spiderman 2</th><th>John Watts</th><th>4/5</th></tr>" in response.data
