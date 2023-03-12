9f0522d90ca9d42dff7a04be2a8d87bce005fbda
eggsandwich-dev <cpitre1@uncc.edu>
2023-03-12 04:56:05 -0400

    method to create movie objects

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -23,10 +23,10 @@ def create_movies_form():
     return render_template('create_movies_form.html', create_rating_active=True)
 
 
-@app.post('/movies')
+@app.post('/movies/new')
 def create_movie():
-    # TODO: Feature 2
-    # After creating the movie in the database, we redirect to the list all movies page
+    get_movie_repository().create_movie(request.form["name"], request.form["director"], request.form["ratingOptions"])
+    
     return redirect('/movies')
 
 
