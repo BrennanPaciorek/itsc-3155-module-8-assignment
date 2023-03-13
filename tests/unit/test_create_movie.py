c3819e6866598c1e2041e4a5f75d3beb25eada39
eggsandwich-dev <cpitre1@uncc.edu>
2023-03-13 02:28:30 -0400

    refactored test_create_movie

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,25 +1,10 @@
-from src.repositories.movie_repository import get_movie_repository as mr
+from src.repositories.movie_repository import get_movie_repository
 from src.models.movie import Movie
 
 def test_create_movie():
-    title1 = "Star Wars"
-    director1 = "George Lucas"
-    rating1 = 5
+    test_movie = get_movie_repository().create_movie("Star Wars", "George Lucas", 5)
 
-    movie1 = Movie(title1, director1, rating1)
-    test_movie = mr().create_movie(title1, director1, rating1)
-
-    title2 = "Titanic"
-    director2 = "James Cameron"
-    rating2 = 4
-
-    movie2 = Movie(title2, director2, rating2)
-
-    assert test_movie.title == movie1.title
-    assert test_movie.director == movie1.director
-    assert test_movie.rating == movie1.rating
-
-    assert test_movie.title != movie2.title
-    assert test_movie.director != movie2.director
-    assert test_movie.rating != movie2.rating
-
+    assert type(test_movie) == Movie
+    assert test_movie.title == "Star Wars"
+    assert test_movie.director == "George Lucas"
+    assert test_movie.rating == 5
