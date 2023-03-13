4acc69ae018be769ddc7dab812946e2b636ef8a4
eggsandwich-dev <cpitre1@uncc.edu>
2023-03-13 02:06:43 -0400

    created test_create_movies_page.py e2e test

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,14 @@
 # TODO: Feature 2
+from flask.testing import FlaskClient
+from src.models.movie import Movie
+
+def test_create_movies_page(test_app: FlaskClient):
+    response = test_app.post('/movies', data={
+        "name": "Star Wars",
+        "director": "George Lucas",
+        "ratingOptions": "5"
+    })
+
+    assert response.status_code == 302
+    print(response.get_data(True))
+
