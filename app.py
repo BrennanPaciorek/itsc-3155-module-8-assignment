4e65b3a3641acd8bb6995c3ab069f0b86bd2ec3d
Zach Sanford <99988194+zwsanford@users.noreply.github.com>
2023-03-14 14:51:38 -0400

    Feature 1 script
    fix html to show rating, and add the movie_list function into effect

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def index():
 
 @app.get('/movies')
 def list_all_movies():
-    # TODO: Feature 1
-    return render_template('list_all_movies.html', list_movies_active=True)
+    movie_list = movie_repository.get_all_movies()
+    return render_template('list_all_movies.html', list_movies_active=True, movie_list=movie_list)
 
 
 @app.get('/movies/new')
