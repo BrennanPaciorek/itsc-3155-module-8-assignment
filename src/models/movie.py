078ee10f9826bf62aa218ac374d60e253f729b31
aslanka <49008222+aslanka@users.noreply.github.com>
2023-03-14 21:52:04 -0400

    implemented feature 3

--- src/models/movie.py
+++ src/models/movie.py
@@ -1,12 +1,12 @@
 class Movie:
     """A movie holds a title, director, and rating"""
 
-    def __init__(self, title: str, director: str, rating: int) -> None:
+    def __init__(self, title: str, director: str, rating: int):
         self.title = title
         self.director = director
         self.rating = rating
 
-    def get_title():
+    def get_title(self):
         return self.title
     
     def toString(self):
