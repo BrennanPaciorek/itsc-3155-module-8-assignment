6d4970312f05c17ee4dc7a5480fbb84623129b25
Quinn Harris <quinnharris0925@gmail.com>
2023-03-16 19:51:17 -0400

    e2e testing added

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,9 @@
 # TODO: Feature 1
+def test_all_movies_page(test):
+    test.post('/movies', data=dict(title="Batman Begins", director="Christopher Nolan", rating="4"))
+    test.post('/movies', data=dict(title="Creed", director="Ryan Coogler", rating="3"))
+    test.post('/movies', data=dict(title="Django", director="Quentin Tarantino", rating="4"))
+    response = test.get('/movies')
+    assert b"<tr><th>Batman Begins</th><td>Christopher Nolan</td><td>4</td></tr>" in response.data
+    assert b"<tr><th>Creed</th><td>Ryan Coogler</td><td>3</td></tr>" in response.data
+    assert b"<tr><th>Django</th><td>Quentin Tarantino</td><td>4</td></tr>" in response.data
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,18 @@
-# TODO: Feature 1
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+from src.repositories.movie_repository import _movie_repo
+from src.models.movie import Movie
+#Used chatGPT to find out how to have access to the movie repo and get all movies function
+
+def test_get_all_movies_method():
+    movies = [Movie("Batman Begins", "Christopher Nolan", 4), Movie("Creed", "Ryan Coogler", 3), Movie("Django", "Quentin Tarantino", 4)]
+    _movie_repo.create_movie("Batman Begins", "Christopher Nolan", 4)
+    _movie_repo.create_movie("Creed", "Ryan Coogler", 3)
+    _movie_repo.create_movie("Django", "Quentin Tarantino", 4)
+    test_movies = _movie_repo.get_all_movies()
+    for movie in range(3):
+        assert type(test_movies[movie]) is Movie
+        assert test_movies[movie].title == movies[movie].title
+        assert test_movies[movie].director == movies[movie].director
+        assert test_movies[movie].rating == movies[movie].rating
