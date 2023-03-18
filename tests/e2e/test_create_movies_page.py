56f3dba96ab39ed05788a3c36a9781397ce3d946
carsonj741 <carsonjacobus26@gmail.com>
2023-03-18 19:21:44 -0400

    Added Tests for Part 2

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,15 @@
 # TODO: Feature 2
+from src.repositories.movie_repository import movie_repository
+from src.repositories.movie_repository import Movie
+
+def test_create_movies(test_app):
+    test = {
+        'title' : 'Paul Blaurt the mall cop',
+        'director' : 'carson jacobus',
+        'rating' : '3'
+    }
+    answer = test_app.post('/movies',data = test)
+
+    assert movie_repository.get_movie_by_title('Paul Blaurt the mall cop') != None
+    assert answer.status_code == 302
+    assert answer.request.path == "/movies"
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,12 @@
 # TODO: Feature 2
+from src.repositories.movie_repository import movie_repository
+from src.models.movie import Movie
+
+def test_create_movie():
+    movie = movie_repository.create_movie('Paul Blaurt the mall cop','carson jacobus',3)
+
+    assert type(movie) == Movie
+    assert movie.title == 'Paul Blaurt the mall cop'
+    assert movie.directior == 'carson jacobus'
+    assert movie.rating == 3
+    assert movie_repository.get_movie_by_title('Paul Blaurt the mall cop') != None
